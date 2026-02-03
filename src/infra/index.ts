5eb56c22f9e1d938582fcc3a9bbcf9b67af106bd
x7c1 <jmtaro+x7c1@gmail.com>
2026-02-03 17:08:48 +0000

    refactor(libs): move reloader from infra to libs layer

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -7,6 +7,5 @@ export * from './drag/index.js';
 export * from './file/index.js';
 export * from './gsettings/index.js';
 export * from './monitor/index.js';
-export * from './reloader/index.js';
 export * from './shortcuts/index.js';
 export * from './window/index.js';
--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,4 +1,5 @@
 // Libs Layer
 // Contains generic utilities that are not specific to snappa domain
 
+export * from './reloader/index.js';
 export * from './uuid/index.js';
